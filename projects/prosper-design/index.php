bd97de042ab05205dfb0a2951a02c9ad1e7b5990
prosp <prosperdesignweb@gmail.com>
2020-05-16 14:15:04 -0300

    Atualização do Projeto Site Prosper Design

--- projects/prosper-design/index.php
+++ projects/prosper-design/index.php
@@ -4,21 +4,18 @@
         <meta charset="UTF-8">
         <title>Prosper Design</title>
         <meta name="viewport" content="width=device-width, intial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" type="text/css" href="../prosper-design/assets/css/boot.css">
         <link rel="stylesheet" type="text/css" href="../prosper-design/assets/css/style.css">
     </head>
     <body>
         <h1>Site Prosper Design</h1>
-        <div class="container">
-            <p>lorem ipsum esorjco chlsoriuooo hd89schwpsco</p>
-            
-        </div>
+        
         <?php
         // put your code here
         ?>
     </body>
     <footer>
-        
+
     </footer>
 </html>
